ab42cd67b41329c1696f32ebde212859c838754f
Daniel Andrade <daniel@danielandrade.net>
2019-01-30 19:42:23 +0700

    Fix mouse, keyboard and touchscreen.

--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/bash
-echo "Boom 6"
+echo "Boom 5"
 sleep 5
+
+export DISPLAY=:0.0
+export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+
+# echo "xset s off" >> /etc/X11/xinit/xinitrc
+# echo "xset -dpms" >> /etc/X11/xinit/xinitrc
+# echo "xset s noblank" >> /etc/X11/xinit/xinitrc
+
 startx
+# xinit /usr/bin/xterm -geometry 80x20+100-50
