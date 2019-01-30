22d16654494f5063d62845dedeff5980da1f42f3
Daniel Andrade <daniel@danielandrade.net>
2019-01-30 17:55:04 +0700

    Test with xfce4

--- start.sh
+++ start.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/bash
-echo "Boom 5"
+echo "Boom 6"
 sleep 5
-# startx
-echo "xset s off" >> /etc/X11/xinit/xinitrc
-echo "xset -dpms" >> /etc/X11/xinit/xinitrc
-echo "xset s noblank" >> /etc/X11/xinit/xinitrc
-
-xinit /usr/bin/xterm -geometry 80x20+100-50
+startx
