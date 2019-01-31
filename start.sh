16b25c0b7c7cf681a5a2eed6c3aa5f559135ac98
Daniel Andrade <daniel@danielandrade.net>
2019-01-31 15:30:45 +0700

    Small refactor

--- start.sh
+++ start.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/bash
-echo "Boom 5"
-sleep 5
-
+echo "Boom 7 "
 export DISPLAY=:0.0
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
-
-# echo "xset s off" >> /etc/X11/xinit/xinitrc
-# echo "xset -dpms" >> /etc/X11/xinit/xinitrc
-# echo "xset s noblank" >> /etc/X11/xinit/xinitrc
-
-startx
-# xinit /usr/bin/xterm -geometry 80x20+100-50
+startx 
