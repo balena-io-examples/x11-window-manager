584fe38242b42815b6558d9c30baa01c17721f88
Daniel Andrade <daniel@danielandrade.net>
2019-03-07 13:02:15 +0000

    General fixes

--- start_x86.sh
+++ start_x86.sh
@@ -6,7 +6,7 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 # rotate screen if env variable is set [normal, inverted, left or right]
 if [[ ! -z "$ROTATE_DISPLAY" ]]; then
   echo "YES"
-  (sleep 5 && DISPLAY=:0 xrandr -o left) & 
+  (sleep 3 && DISPLAY=:0 xrandr -o $ROTATE_DISPLAY) & 
 fi
 
 # start desktop manager
